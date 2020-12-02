1d553b0edca2ba0c9d2452e5af2ec5a7ff808c09
Denis Willian <deniswliborges@gmail.com>
2020-12-02 00:05:27 -0200

    Testando a primeira classe de dominio  - Categoria

---
+++ src/main/java/com/deniswillian/cursomcc/domain/Categoria.java
@@ -0,0 +1,72 @@
+package com.deniswillian.cursomcc.domain;
+
+import java.io.Serializable;
+
+// 1- Criando Endpoint("Tabela")   7- Implementando o Serializable : Para ser gravados em arquivos
+public class Categoria implements Serializable{
+	
+		//8- Coloque o mouse em cima da linha 6 e selecione opção | Add default serial version ID 
+		private static final long serialVersionUID = 1L;
+	
+		// 2- Criando Atributos do Endpoint(tabela)
+	private Integer id;
+	private String nome;
+	
+	// 3- Criando os CONSTRUTORES. Obs: Não incluir coleções no construtor com parâmetros
+	public Categoria() {
+	}
+
+	// 4- Criando construtores com ATRIBUTOS com botão direito clicar em 1-Source, 2-Constructor using Fields
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	// 5- Criando GET e SET
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	//6- Gerar o HASCODE e EQUALs : Comparar/relacionar os objetos ou tabelas  por valor, ou seja, pela Chave Primaria (PK). Obs: Escolha o ID para relacionar as tabelas
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	//6.1- Gerar o HASCODE e EQUALs : Comparar/relacionar os objetos ou tabelas  por valor, ou seja, pela Chave Primaria (PK). Obs: Escolha o ID para relacionar as tabelas
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+}
--- src/main/java/com/deniswillian/cursomcc/resources/CategoriaResource.java
+++ src/main/java/com/deniswillian/cursomcc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.deniswillian.cursomcc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.deniswillian.cursomcc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria>listar() {
+		
+		//Class Domain e inserindo os dados nos Construtores (atributos)
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritorio ");
+		
+		//Ctrl+Shif+O e selecione opção java.util.list
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 	
 }
