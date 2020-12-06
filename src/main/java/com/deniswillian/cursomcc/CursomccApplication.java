ed163b227ed84f8ec2bf7a5813fb7b8c7fe73a55
Denis Willian <deniswliborges@gmail.com>
2020-12-06 00:49:17 -0200

    Criando operação instanciação

--- src/main/java/com/deniswillian/cursomcc/CursomccApplication.java
+++ src/main/java/com/deniswillian/cursomcc/CursomccApplication.java
@@ -1,13 +1,31 @@
 package com.deniswillian.cursomcc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.deniswillian.cursomcc.domain.Categoria;
+import com.deniswillian.cursomcc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomccApplication {
+public class CursomccApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomccApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 =new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
