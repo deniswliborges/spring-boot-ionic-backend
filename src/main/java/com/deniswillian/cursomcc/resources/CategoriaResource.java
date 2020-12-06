8855d917d59d0e72cc1c625b1b33abf24ffa5849
Denis Willian <deniswliborges@gmail.com>
2020-12-05 23:47:46 -0200

    Criando repository e service para Categoria

--- src/main/java/com/deniswillian/cursomcc/resources/CategoriaResource.java
+++ src/main/java/com/deniswillian/cursomcc/resources/CategoriaResource.java
@@ -1,30 +1,25 @@
 package com.deniswillian.cursomcc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.deniswillian.cursomcc.domain.Categoria;
+import com.deniswillian.cursomcc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
-
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria>listar() {
-		
-		//Class Domain e inserindo os dados nos Construtores (atributos)
-		Categoria cat1 = new Categoria(1,"Informatica");
-		Categoria cat2 = new Categoria(2,"Escritorio ");
-		
-		//Ctrl+Shif+O e selecione opção java.util.list
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		return lista;
-	}
 	
+	@Autowired // Instanciar automaticamente a Class Service
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?>find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
---
+++ src/main/java/com/deniswillian/cursomcc/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.deniswillian.cursomcc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.deniswillian.cursomcc.domain.Categoria;
+import com.deniswillian.cursomcc.repositories.CategoriaRepository;
+import com.deniswillian.cursomcc.services.exceptions.ObjectNotFounfException;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFounfException(
+				"Objeto não encontrado! Id: "+ id + ", Tipo: "+ Categoria.class.getName()));
+	}
+	
+}
