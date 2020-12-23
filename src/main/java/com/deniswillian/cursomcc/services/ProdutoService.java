ebc5bead392b85dae6945b9c4f20b55edaad4dc8
Denis Willian <deniswliborges@gmail.com>
2020-12-23 16:15:12 -0200

    Busca de produtos por nome e categorias

--- src/main/java/com/deniswillian/cursomcc/services/ProdutoService.java
+++ src/main/java/com/deniswillian/cursomcc/services/ProdutoService.java
@@ -1,10 +1,17 @@
 package com.deniswillian.cursomcc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+
+import com.deniswillian.cursomcc.domain.Categoria;
 import com.deniswillian.cursomcc.domain.Produto;
+import com.deniswillian.cursomcc.repositories.CategoriaRepository;
 import com.deniswillian.cursomcc.repositories.ProdutoRepository;
 import com.deniswillian.cursomcc.services.exceptions.ObjectNotFounfException;
 
@@ -14,12 +21,21 @@ import com.deniswillian.cursomcc.services.exceptions.ObjectNotFounfException;
 public class ProdutoService {
 
 	@Autowired
-	private ProdutoRepository repo2;
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	
 	public Produto find(Integer id) {
-		Optional<Produto> obj = repo2.findById(id);
+		Optional<Produto> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFounfException(
 				"Objeto não encontrado! Id: "+ id + ", Tipo: "+ Produto.class.getName()));
 	}
 	
+	
+	public Page<Produto> search(String nome, List<Integer> ids,Integer page, Integer linesPerPage, String orderBy, String direction) {
+			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+			List<Categoria> categorias = categoriaRepository.findAllById(ids);
+			return repo.findDistincByNomeContainingAndCategoriasIn(nome,categorias,pageRequest);
+	}
 }
