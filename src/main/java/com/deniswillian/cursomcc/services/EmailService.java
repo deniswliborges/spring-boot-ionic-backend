d815e5d43461eb276951be98670013669bafdee2
Denis Willian <deniswliborges@gmail.com>
2020-12-30 21:49:47 -0200

    Email HTML

--- src/main/java/com/deniswillian/cursomcc/services/EmailService.java
+++ src/main/java/com/deniswillian/cursomcc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.deniswillian.cursomcc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.deniswillian.cursomcc.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
