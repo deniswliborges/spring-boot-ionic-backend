70110aa7d1e0d95e13ef6952eb4e319f60b4316b
Denis Willian <deniswliborges@gmail.com>
2020-12-30 23:58:20 -0200

    Salvando perfis de usuário na base de dados

--- src/main/java/com/deniswillian/cursomcc/domain/Cliente.java
+++ src/main/java/com/deniswillian/cursomcc/domain/Cliente.java
@@ -5,17 +5,20 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.deniswillian.cursomcc.domain.enums.Perfil;
 import com.deniswillian.cursomcc.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -45,12 +48,17 @@ public class Cliente implements Serializable {
 	@ElementCollection // Comando para apontar a tabela comum, ou seja, somente com 1 atributo
 	@CollectionTable(name = "TELEFONE") // Comando para criar a Endpoint(Tabela) TELEFONE
 	private Set<String> telefones = new HashSet<>();
+	
+	@ElementCollection(fetch=FetchType.EAGER)
+	@CollectionTable(name="PERFIS")
+	private Set<Integer> perfis = new HashSet<>();
 
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
@@ -61,6 +69,7 @@ public class Cliente implements Serializable {
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = (tipo == null) ? null : tipo.getCod();
 		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Integer getId() {
@@ -111,6 +120,13 @@ public class Cliente implements Serializable {
 		this.senha = senha;
 	}
 
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+
+	public void addPerfil(Perfil perfil) {
+		perfis.add(perfil.getCod());
+	}
 	public List<Endereco> getEnderecos() {
 		return enderecos;
 	}
