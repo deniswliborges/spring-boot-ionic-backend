a29d88888d68fda7b9c8350502cc2f4500eab318
Denis Willian <deniswliborges@gmail.com>
2020-12-30 23:35:06 -0200

    Adicionando senha a Cliente

--- src/main/java/com/deniswillian/cursomcc/dto/ClienteNewDTO.java
+++ src/main/java/com/deniswillian/cursomcc/dto/ClienteNewDTO.java
@@ -22,13 +22,16 @@ public class ClienteNewDTO implements Serializable {
 	private String email;
 
 	@NotEmpty(message = "Preenchimento obrigatório")
-
 	private String cpfOuCnpj;
 
 	private Integer tipo;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	private String senha;
 
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String logradouro;
+	
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String numero;
 
@@ -150,6 +153,14 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 }
