af8dd20c0e03e2e9c715b3dbd8cb542e8e3a2010
Denis Willian <deniswliborges@gmail.com>
2021-01-06 18:07:49 -0200

    Salvando primeiro arquivo S3

--- src/main/java/com/deniswillian/cursomcc/CursomccApplication.java
+++ src/main/java/com/deniswillian/cursomcc/CursomccApplication.java
@@ -1,48 +1,25 @@
 package com.deniswillian.cursomcc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.deniswillian.cursomcc.domain.Categoria;
-import com.deniswillian.cursomcc.domain.Cidade;
-import com.deniswillian.cursomcc.domain.Cliente;
-import com.deniswillian.cursomcc.domain.Endereco;
-import com.deniswillian.cursomcc.domain.Estado;
-import com.deniswillian.cursomcc.domain.ItemPedido;
-import com.deniswillian.cursomcc.domain.Pagamento;
-import com.deniswillian.cursomcc.domain.PagamentoComBoleto;
-import com.deniswillian.cursomcc.domain.PagamentoComCartao;
-import com.deniswillian.cursomcc.domain.Pedido;
-import com.deniswillian.cursomcc.domain.Produto;
-import com.deniswillian.cursomcc.domain.enums.EstadoPagamento;
-import com.deniswillian.cursomcc.domain.enums.TipoCliente;
-import com.deniswillian.cursomcc.repositories.CategoriaRepository;
-import com.deniswillian.cursomcc.repositories.CidadeRepository;
-import com.deniswillian.cursomcc.repositories.ClienteRepository;
-import com.deniswillian.cursomcc.repositories.EnderecoRepository;
-import com.deniswillian.cursomcc.repositories.EstadoRepository;
-import com.deniswillian.cursomcc.repositories.ItemPedidoRepository;
-import com.deniswillian.cursomcc.repositories.PagamentoRepository;
-import com.deniswillian.cursomcc.repositories.PedidoRepository;
-import com.deniswillian.cursomcc.repositories.ProdutoRepository;
+import com.deniswillian.cursomcc.services.S3Service;
 
 @SpringBootApplication
 public class CursomccApplication implements CommandLineRunner{
 	
-	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomccApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
-		
-	}
+	public void run(String... args) throws Exception {	
+		s3Service.uploadFile("C:\\Users\\Administrador\\Documents\\PARTICULARES\\SpringBoot\\fotos\\ana.jpg");
+	}	
 
 }
